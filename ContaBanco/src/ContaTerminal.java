f2d7f0482142202b05d25c486471721490f5c230
Vincius Von <vini.r.faria@hotmail.com>
2024-08-29 12:15:37 -0300

    implementação do sistema bancario

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,7 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+    /* O programa é uma simulação de um sistema de registro bancario. */
+     
+       
+        int numero;
+        double saldo;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("digite o numero de sua Agência: ");
+        numero = scanner.nextInt();
+
+        System.out.println("digite a sua agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("digite seu saldo");
+        saldo = scanner.nextDouble();
+
+
+
+        System.out.println("Olá " + nome + " , obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " , conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
+
+
+
+
+    
     }
 
 }
